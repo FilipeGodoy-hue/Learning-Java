df089d73334d22ea05a2ea75d544d3c3b23fc105
Filipe Godoy <luis.godoy@upe.br>
2026-08-17 14:59:48 -0300

    Initial coding

---
+++ MyfirstProject/src/Main.java
@@ -0,0 +1,12 @@
+public class Main { //Espécie de int main (nao precisa do return 0 no final!!!)
+    //Esse é o meu primeiro código em Java
+    static void main(String[] args){
+    System.out.println("I like hamburger!"); //Aqui, escrevemos o ln depois do print, ficando println para quebrar a linha(mesmo efeito da \n em C)
+    System.out.println("Im studying Java right now!");
+    System.out.println("Im a student at Universidade de Pernambuco");
+    System.out.print("Hello World!");
+
+    // Ao escrever sout+tab , você escreve System.out.println
+
+    }
+}
