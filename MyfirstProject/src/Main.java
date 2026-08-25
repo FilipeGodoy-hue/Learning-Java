7dce243cf0cfe88e090e34d76eb2e97379e84d56
Filipe Godoy <luis.godoy@upe.br>
2026-08-25 18:59:52 -0300

    oi

--- MyfirstProject/src/Main.java
+++ MyfirstProject/src/Main.java
@@ -5,6 +5,10 @@ public class Main { //Espécie de int main (nao precisa do return 0 no final!!!)
     System.out.println("Im studying Java right now!");
     System.out.println("Im a student at Universidade de Pernambuco");
     System.out.print("Hello World!");
+        System.out.println("I like hamburger!"); //Aqui, escrevemos o ln depois do print, ficando println para quebrar a linha(mesmo efeito da \n em C)
+        System.out.println("Im studying Java right now!");
+        System.out.println("Im a student at Universidade de Pernambuco");
+        System.out.print("Hello World!");
 
     // Ao escrever sout+tab , você escreve System.out.println
 
